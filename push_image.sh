719dd322abba62c1b1795a616039de625c543352
Gao JiaXiang <gaojxcs@gmail.com>
2019-12-25 01:53:41 -0800

    fix push image version

--- push_image.sh
+++ push_image.sh
@@ -1,7 +1,7 @@
 set -e
 
 image_name="spark-on-k8s"
-image_version=2.4.4-2.12
+image_version=2.4.4-2.12-v2
 image_name=$image_name:$image_version
 
 docker tag $image_name $HW_IMAGE_PREFIX/$image_name
